77e2427ebf93af68c603dc4a7fea066aa226767f
Aymane Attat <ayman1969@outlook.com>
2023-07-03 09:38:20 +0100

    add vee-validate

---
+++ src/components/VeeValidate.vue
@@ -0,0 +1,41 @@
+<template lang="">
+    <div>
+        <Form @submit="onSubmit" :validation-schema="rulesForm">
+            <label for="email">email</label>
+            <Field class="form-control" name="email" type="email" />
+            <ErrorMessage name="email" />
+
+            <label for="name">name</label>
+            <Field class="form-control" name="name" type="name" />
+            <ErrorMessage name="name" />
+
+            <button class="btn btn-sm btn-primary" type="submit">Add</button>
+        </Form>
+    </div>
+</template>
+<script>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { object, string } from 'yup';
+
+export default {
+    components: {
+        Form,
+        Field,
+        ErrorMessage
+    },
+    data() {
+        return {
+            rulesForm: {}
+        }
+    },
+    created() {
+        this.rulesForm = object({
+            name: string().min(6).required(),
+            email: string().email().required()
+        })
+    }
+}
+</script>
+<style>
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import VeeValidate from '@/components/VeeValidate.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/vee-validate',
+    name: 'veevalidate',
+    component: VeeValidate
   },
   {
     path: '/translate',
